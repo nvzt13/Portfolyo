1ddcdddbd48a4f336687315ae60714d07a676ba0
nvzt13 <nevzatatalay79@gmail.com>
2024-10-29 20:39:21 +0300

    update

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
-const talkButton = document.getElementById("talk-btn")
-const body = document.getElementsByTagName("body")
-const content = document.getElementById("content")
-const submit = document.getElementById("submit")
-console.log(content)
+const viewCv = document.getElementById('view-cv');
+const cv = document.getElementById('cv');
 
-submit.addEventListener("click", (e) => {
-    e.preventDefault()
-    console.log("I am clicked!")
+viewCv.addEventListener('click', () => {
+  cv.classList.toggle('hidden')
 })
